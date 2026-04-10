f312eef3bdaa0c7118d32266d44522ab3e5ee06a
Ivan Mubai <ivan.mubai2012rgmail.com>
2026-04-10 12:37:56 +0100

    fixed database issue and reset migrations

--- news/models.py
+++ news/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
