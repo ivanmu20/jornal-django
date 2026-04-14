b4b53524ab951b4cb409a766748f322b574feb86
ivanmu20 <ivan.mubai2012@gmail.com>
2026-04-14 11:47:31 +0100

    Fix de urls e allow hosts

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-un3fmyc!@%mil#4!)xd#55c9=ux1=1z**trq=v7$i^td83)_5&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['jornal-django.onrender.com']
+ALLOWED_HOSTS = ['jornal-django.onrender.com', '127.0.0.1']
 
 
 # Application definition
