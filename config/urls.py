1729defdd0b088563dc29e4e081f7226f81bac9b
ivanmu20 <ivan.mubai2012@gmail.com>
2026-04-14 11:53:21 +0100

    fix: redirect home to news

--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include 
+from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('/news/') 
 
 urlpatterns = [
+    path('',home),
     path('admin/', admin.site.urls),
     path('news/', include('news.urls')),
 ]
